1d50da8de296c688fdf46acc17053774a42ee034
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-30 22:33:18 +0530

    Create examples.py

---
+++ 47_Markdown to HTML_Converter/examples.py
@@ -0,0 +1,8 @@
+from file1_markdown_to_html import convert_md_to_html
+
+convert_md_to_html(
+    input_md="README.md",
+    output_html="README.html"
+)
+
+print("Markdown converted to HTML successfully.")
