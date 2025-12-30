8624142d87720e820708ba63d96ebac90966c070
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-30 19:05:28 +0530

    Create examples.py

---
+++ 17_text_summarizer/examples.py
@@ -0,0 +1,8 @@
+from solution import summarize_text
+
+text = """Artificial Intelligence is transforming industries by enabling
+machines to learn from data and make decisions."""
+summary = summarize_text(text)
+
+print("Summary:")
+print(summary)
