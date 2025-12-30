78a605bf19235a48c20ce623e89c0fe50e9af023
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-30 22:35:40 +0530

    Create examples.py

---
+++ 51_Image_Watermark/examples.py
@@ -0,0 +1,9 @@
+from file1_image_watyermark import add_watermark
+
+add_watermark(
+    image_path="photo.jpg",
+    watermark_text="© Sandy",
+    output_path="watermarked_photo.jpg"
+)
+
+print("Watermark added successfully.")
